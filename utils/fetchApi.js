493d5ebe2d7c7483c672c04f1bd242394e2f827c
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-03-07 18:15:03 +0530

    integrated bayut api

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseUrl = 'https://bayut.p.rapidapi.com';
+
+
+export const fetchApi = async (url) => {
+    const response = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '31f791c0f3msh1988cd4f33dba7bp179aa6jsn74a333ae24ef'
+          }
+        
+    })
+}
