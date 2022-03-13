b5edec5d2c0405d393057654467f34c536d0eece
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-03-14 03:59:15 +0530

    fetched API

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,14 +1,16 @@
 import axios from "axios";
+// import { dataIndexAttribute } from "react-horizontal-scrolling-menu/dist/types/constants";
 
-const baseUrl = 'https://bayut.p.rapidapi.com';
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 
 export const fetchApi = async (url) => {
-    const response = await axios.get((url), {
+    const {data} = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
             'x-rapidapi-key': '31f791c0f3msh1988cd4f33dba7bp179aa6jsn74a333ae24ef'
           }
         
-    })
+    });
+    return data;
 }
